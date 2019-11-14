1b961181415a7ff545fccbb73454a5210063d76e
Paulo Caldas <paulocaldas90@gmail.com>
2019-11-14 15:52:08 +0100

    run from command line modules

--- tracking_analysis_v2.0/bkg_func/core.py
+++ tracking_analysis_v2.0/bkg_func/core.py
@@ -45,8 +45,8 @@ def plot_trajectories(table_tracks):
     fig, ax = plt.subplots(figsize = (4,4), dpi = 120)
     plt.xlabel('x (microns)', fontsize=10)
     plt.ylabel('y (microns)', fontsize=10)
-    plt.xlim([0,55])
-    plt.ylim([0,55])
+    #plt.xlim([0,55])
+    #plt.ylim([0,55])
     
     for groups, columns in table_tracks.groupby('TRACK_ID'):
         plt.plot(columns['POSITION_X'],columns['POSITION_Y'], lw = 1)
